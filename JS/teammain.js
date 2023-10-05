ff82fe5be399737785bfe320098572407bcadbcd
sengna <jeonseino_o@naver.com>
2023-10-05 16:16:25 +0900

    [feat] 팀 메인페이지....흐읍..

---
+++ JS/teammain.js
@@ -0,0 +1,67 @@
+function ajaxHandler (method, url, target) {
+    console.log(url)
+
+    const xhttp = new XMLHttpRequest()
+    xhttp.addEventListener('readystatechange', function() {
+        // alert(this.readyState + ":" + this.status)
+
+        if (this.readyState == 4 ) {
+            if(this.status == 200) {
+                target.innerHTML = this.responseText
+            } else if(this.status == 404) {
+                alert('자원이 없습니다.')
+            } else if(this.status == 500) {
+                alert('서버 실행시 오류가 발생했습니다.')
+            } else {
+                alert(this.responseText)
+            } // if-else
+        } // if
+    }) // xhttp.addEventListener()
+    xhttp.open(method, url)
+    xhttp.send()
+} // ajaxHandler
+
+window.addEventListener('load', () => {
+
+    // DOM Tree에서 section 객체 찾기
+    const sectionObj = document.querySelector('section')
+    // DOM Tree에서 nav>ul>li>a 객체들 찾기
+    const menus = document.querySelectorAll('nav>ul>li>a')
+    
+    menus.forEach((menu, index) => {
+
+        // 〓〓 메뉴 객체에서 클릭이벤트가 발생했을 때 할 일 START 〓〓
+        menu.addEventListener('click', (e) => {
+            console.log(e.target.className)
+            // menu
+            switch(e.target.className) { // 화살표 함수 내부에서의 this는 윈도우 객체이기 때문에 e.target 사용!
+                case 'noticeBoard':
+                    ajaxHandler('GET', '#', sectionObj) // sectionObj = 결과를 보여줄 영역
+                    break;
+
+                case 'taskBoard':
+                    ajaxHandler('GET', '#', sectionObj)
+                    break;
+                    
+                case 'QnABoard':
+                    ajaxHandler('GET', '#', sectionObj)
+                    break;
+
+                case 'attendencePage':
+                    ajaxHandler('GET', '#', sectionObj)
+                    break;
+
+                case 'rankPage': 
+                    ajaxHandler('GET', '#', sectionObj)
+                    break;
+
+                case 'manageTeamPage': 
+                    ajaxHandler('GET', '#', sectionObj)
+                    break;
+            } // switch(e.target.class)()
+            e.preventDefault()
+
+        }) // menu.addEventListener()
+        // 〓〓 메뉴 객체에서 클릭이벤트가 발생했을 때 할 일 END 〓〓
+
+    }) // menus.forEach()
