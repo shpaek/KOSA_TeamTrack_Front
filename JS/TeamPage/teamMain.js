86a9faa41fa9ef8f59c20b16b6fb2eb05a06a6e7
sengna <jeonseino_o@naver.com>
2023-10-06 00:34:29 +0900

    [feat] 팀 메인페이지 navigation 수정 완료

--- JS/TeamPage/teamMain.js
+++ JS/TeamPage/teamMain.js
@@ -1,16 +1,16 @@
-function ajaxHandler (method, url, target) {
+function ajaxHandler(method, url, target) {
     console.log(url)
 
     const xhttp = new XMLHttpRequest()
-    xhttp.addEventListener('readystatechange', function() {
+    xhttp.addEventListener('readystatechange', function () {
         // alert(this.readyState + ":" + this.status)
 
-        if (this.readyState == 4 ) {
-            if(this.status == 200) {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
                 target.innerHTML = this.responseText
-            } else if(this.status == 404) {
+            } else if (this.status == 404) {
                 alert('자원이 없습니다.')
-            } else if(this.status == 500) {
+            } else if (this.status == 500) {
                 alert('서버 실행시 오류가 발생했습니다.')
             } else {
                 alert(this.responseText)
@@ -26,15 +26,15 @@ window.addEventListener('load', () => {
     // DOM Tree에서 section 객체 찾기
     const sectionObj = document.querySelector('section')
     // DOM Tree에서 nav>ul>li>a 객체들 찾기
-    const menus = document.querySelectorAll('nav>ul>li>a')
-    
+    const menus = document.querySelectorAll('nav>ul>li>a, nav>ul>li>ul>li>a')
+
     menus.forEach((menu, index) => {
 
         // 〓〓 메뉴 객체에서 클릭이벤트가 발생했을 때 할 일 START 〓〓
         menu.addEventListener('click', (e) => {
             console.log(e.target.className)
             // menu
-            switch(e.target.className) { // 화살표 함수 내부에서의 this는 윈도우 객체이기 때문에 e.target 사용!
+            switch (e.target.className) { // 화살표 함수 내부에서의 this는 윈도우 객체이기 때문에 e.target 사용!
                 case 'noticeBoard':
                     ajaxHandler('GET', './notice.html', sectionObj) // sectionObj = 결과를 보여줄 영역
                     break;
@@ -42,7 +42,7 @@ window.addEventListener('load', () => {
                 case 'taskBoard':
                     ajaxHandler('GET', './taskmain.html', sectionObj)
                     break;
-                    
+
                 case 'QnABoard':
                     ajaxHandler('GET', '#', sectionObj)
                     break;
@@ -51,12 +51,24 @@ window.addEventListener('load', () => {
                     ajaxHandler('GET', '#', sectionObj)
                     break;
 
-                case 'rankPage': 
+                case 'rankPage':
                     ajaxHandler('GET', '#', sectionObj)
                     break;
 
-                case 'manageTeamPage': 
+                case 'manageTeamProperties':
                     ajaxHandler('GET', '#', sectionObj)
+                    break;
+
+                case 'manageTeamCurrentMember':
+                    ajaxHandler('GET', './teamManageCurrentMember.html', sectionObj)
+                    break;
+
+                case 'manageTeamApproval':
+                    ajaxHandler('GET', './teamManageApproval.html', sectionObj)
+                    break;
+
+                case 'manageTeamExaminer':
+                    ajaxHandler('GET', './teamManageExaminer.html', sectionObj)
                     break;
 
             } // switch(e.target.class)()
