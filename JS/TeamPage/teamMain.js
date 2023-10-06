53441052b1a8033050c43e64988b31124cd049f6
sengna <jeonseino_o@naver.com>
2023-10-06 10:18:30 +0900

    [Fix] teamMain.js ajax로 변경

--- JS/TeamPage/teamMain.js
+++ JS/TeamPage/teamMain.js
@@ -1,3 +1,4 @@
+/*
 function ajaxHandler(method, url, target) {
     console.log(url)
 
@@ -20,63 +21,80 @@ function ajaxHandler(method, url, target) {
     xhttp.open(method, url)
     xhttp.send()
 } // ajaxHandler
+*/
 
-window.addEventListener('load', () => {
+function ajaxHandler(method, u, target) {
+    console.log(u)
+
+    if (method == 'GET') {
+        target.load(u, function (response, status, xhr) { // jQuery용 메소드 load()
+            if (status == "error") {
+                alert(xhr.status + ShadowRoot.statusText)
+            } // inner-if
+        })  // .load()
+    } // outer-if
+
+} // ajaxHandler
+
+$(() => {
 
     // DOM Tree에서 section 객체 찾기
-    const sectionObj = document.querySelector('section')
+    const $sectionObj = $(`section`)
     // DOM Tree에서 nav>ul>li>a 객체들 찾기
-    const menus = document.querySelectorAll('nav>ul>li>a, nav>ul>li>ul>li>a')
-
-    menus.forEach((menu, index) => {
+    const $menus = $(`nav>ul>li>a, nav>ul>li>ul>li>a`)
 
         // 〓〓 메뉴 객체에서 클릭이벤트가 발생했을 때 할 일 START 〓〓
-        menu.addEventListener('click', (e) => {
+        $menus.click((e) => {
             console.log(e.target.className)
             // menu
             switch (e.target.className) { // 화살표 함수 내부에서의 this는 윈도우 객체이기 때문에 e.target 사용!
                 case 'noticeBoard':
-                    ajaxHandler('GET', './notice.html', sectionObj) // sectionObj = 결과를 보여줄 영역
+                    ajaxHandler('GET', './notice.html', $sectionObj) // sectionObj = 결과를 보여줄 영역
                     break;
 
                 case 'taskBoard':
-                    ajaxHandler('GET', './taskmain.html', sectionObj)
+                    ajaxHandler('GET', './taskmain.html', $sectionObj)
                     break;
 
                 case 'QnABoard':
-                    ajaxHandler('GET', '#', sectionObj)
+                    ajaxHandler('GET', '#', $sectionObj)
                     break;
 
                 case 'attendencePage':
-                    ajaxHandler('GET', '#', sectionObj)
+                    ajaxHandler('GET', '#', $sectionObj)
                     break;
 
                 case 'rankPage':
-                    ajaxHandler('GET', '#', sectionObj)
+                    ajaxHandler('GET', '#', $sectionObj)
                     break;
 
                 case 'manageTeamProperties':
-                    ajaxHandler('GET', '#', sectionObj)
+                    ajaxHandler('GET', '#', $sectionObj)
                     break;
 
                 case 'manageTeamCurrentMember':
-                    ajaxHandler('GET', './teamManageCurrentMember.html', sectionObj)
+                    ajaxHandler('GET', './teamManageCurrentMember.html', $sectionObj)
                     break;
 
                 case 'manageTeamApproval':
-                    ajaxHandler('GET', './teamManageApproval.html', sectionObj)
+                    ajaxHandler('GET', './teamManageApproval.html', $sectionObj)
                     break;
 
                 case 'manageTeamExaminer':
-                    ajaxHandler('GET', './teamManageExaminer.html', sectionObj)
+                    ajaxHandler('GET', './teamManageExaminer.html', $sectionObj)
                     break;
-
             } // switch(e.target.class)()
             e.preventDefault()
-
+    
         }) // menu.addEventListener()
-        // 〓〓 메뉴 객체에서 클릭이벤트가 발생했을 때 할 일 END 〓〓
-
-    }) // menus.forEach()
+        // 〓〓 메뉴 객체에서 클릭이벤트가 발생했을 때 할 일 END 〓〓)
+    
+        // 〓〓 로고 img 객체에서 클릭이벤트가 발생했을 때 할 일 START 〓〓)
+        // const $logo = $(`header>img`)
+    
+        //$logo.click((e) => {
+        //    location.href = `./main.html`
+        //})
+        // 〓〓 로고 img 객체에서 클릭이벤트가 발생했을 때 할 일 END 〓〓)
 
 });
