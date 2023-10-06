10b2ba11a5c7fbe99ca6d90c9804411c01e3a222
nawonhee <65769072+nawonhee@users.noreply.github.com>
2023-10-07 01:32:10 +0900

    [Feat] 공지 수정 추가

--- JS/noticedetail.js
+++ JS/noticedetail.js
@@ -15,9 +15,9 @@ $(()=>{
             const noticeContent = responseJSONObj.noticeContent
             const regDate = responseJSONObj.regDate
 
-            $('div.titleline>h4').html(noticeTitle)
-            $('div.titleline>span').text(regDate)
-            $('div.content>p').html(noticeContent)
+            $('div.detailtitleline>h4').html(noticeTitle)
+            $('div.detailtitleline>span').text(regDate)
+            $('div.detailcontent>p').html(noticeContent)
         },
         error:(jqXHR, textStatus)=>{
             alert(jqXHR.readyState+":"+jqXHR.status+":"+jqXHR.statusText)
@@ -25,7 +25,71 @@ $(()=>{
         }
     })
 
-    $('div.buttons>button.remove').on('click',(e)=>{
+    //---- 수정버튼 클릭 시 발생 이벤트 ----
+    $('div.noticedetail>div.detailbuttons>button.edit').on('click',(e)=>{
+        $('div.noticedetail').hide()
+        $('div.editnotice').show()
+
+        $.ajax({
+            url: backURL+'/noticedetail',
+            method : 'get',
+            data : `teamNo=${teamNo}&noticeNo=${noticeNo}`,
+            success: (responseJSONObj)=>{
+                const noticeTitle = responseJSONObj.noticeTitle
+                const noticeContent = responseJSONObj.noticeContent
+                const mainStatus = responseJSONObj.mainStatus
+                
+                $('div.mainnotice>input[name=status]').attr('value',mainStatus);
+                $('div.modifytitleline>input[name=title]').attr('value',noticeTitle);
+                $('div.modifycontent>input[name=content]').attr('value',noticeContent);
+            },
+            error:(jqXHR, textStatus)=>{
+                alert(jqXHR.readyState+":"+jqXHR.status+":"+jqXHR.statusText)
+                console.log(jqXHR)
+            }
+        })
+    })
+
+    //---- 완료버튼 클릭 시 발생 이벤트 ----
+    $('div.editnotice>form>div.writebuttons>button[type=submit]').on('click',(e)=>{
+        const $formObj = $('form')
+
+        $formObj.submit((e) => {
+            const fd = new FormData(e.target)
+            fd.append("teamNo", teamNo)
+            fd.append("noticeNo", noticeNo)
+
+            $.ajax({
+                xhrFields:{
+                withCredentials : true
+                },
+                url: `${backURL}/editnotice`,
+                method : 'post',
+                contentType: false, //파일첨부용 프로퍼티
+                processData : false, //파일첨부용 프로퍼티
+                data : fd,
+                success : (responseJSONObj)=>{
+                    if(responseJSONObj.status==1){
+                        alert(responseJSONObj.msg)
+                        location.href = `${frontURL}/notice.html?teamNo=${teamNo}`
+                    }else{
+                        alert(responseJSONObj.msg)
+                    }
+                },
+                error: (jqxhr)=>{
+                    alert(jqxhr.status)
+                }
+            })
+            return false
+        })
+    })
+
+    $('div.editnotice>div.backbutton>button[name=back]').on('click',(e)=>{
+        location.href = `${frontURL}/noticedetail.html?teamNo=${teamNo}&noticeNo=${noticeNo}`
+    })
+
+    //---- 삭제버튼 클릭 시 발생 이벤트 ----
+    $('div.noticedetail>div.detailbuttons>button.remove').on('click',(e)=>{
         var result = confirm("삭제하시겠습니까?")
         if(result == true){
             $.ajax({
