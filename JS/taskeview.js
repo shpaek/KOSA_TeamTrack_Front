ef98aefba658ff0f9a6b7ea7258fec2288a1c724
qbobl5 <ddddd991125@gmail.com>
2023-10-10 17:20:15 +0900

    [Feat] 과제뷰어 js 추가

--- JS/taskeview.js
+++ JS/taskeview.js
@@ -9,8 +9,42 @@ function ajaxHandler(method, u, target) {
         })
     }
 }
-
+const backURL = 'http://localhost:8888/KOSA_Project2'
+const taskNo=localStorage.getItem("taskNo")
 $(()=>{
+    
+    $.ajax({
+        xhrFields: {
+            withCredentials: true
+        },
+        url: `${backURL}/viewtask`,
+        method: 'get',
+        data: `taskNo=${taskNo}`,
+        success: (responseJSONObj) => {
+           if(responseJSONObj.status==0) {
+            alert(responseJSONObj.msg)
+            return
+           } else if(responseJSONObj.status==1) {
+            const title=responseJSONObj.title
+            const nickname=responseJSONObj.nickname
+            const regdate=responseJSONObj.regdate
+
+            const $origintitleObj=$('div.taskviewbox>div.tasktitle')
+            const $titleObj=$('<h1>')
+            $titleObj.append(title)
+            $origintitleObj.append($titleObj)
+
+            const $originnicknameObj=$('div.tasknickname')
+            const $nicknameObj=$('<span>')
+            $nicknameObj.append(nickname)
+            $originnicknameObj.append($nicknameObj)
+            console.log(taskNo)
+           }
+
+        }, error : (()=>{
+            alert('error')
+        })
+    })
     const $menus = $('div.taskboardmenu>ul>li>a')
 
     $menus.click((e) => {
