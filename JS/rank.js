1d42f91d603b432493d417b26fc5b72eb7460af4
ykchoi7 <7dbsrud7@gmail.com>
2023-10-12 17:12:51 +0900

    [feat]rank.html

--- JS/rank.js
+++ JS/rank.js
@@ -4,19 +4,37 @@ const frontURL = 'http://localhost:5500/KOSA_Project2_Front/HTML/'
 $(() => {
     const urlParams = new URL(location.href).searchParams
     const teamNo = urlParams.get('teamNo')
+    const month = urlParams.get('month')
     const selectObj = $('#monthSelect')
     // const teamNo = 9999
     
-    //팀내 개인랭킹 클릭했을 때 랭킹 목록 실행
+    //---- 팀 내 개인랭킹 클릭했을 때 발생할 일 START ----
+    
     $.ajax({
         xhrFields: {
             withCredentials: true 
         },
         url: backURL + '/rank',
         method: 'get',
-        data: `teamNo=${teamNo}`,
+        data: `teamNo=${teamNo}&month=${month}`,
         success: (responseJSONObj) => { //응답데이터 전달
             
+            //select box에 팀에 있는 월들만 표시되도록 실행
+            //원본 객체 찾기 
+            const $originOption = $('#monthSelect>option')
+            const $selectObj = $('#monthSelect')
+
+            responseJSONObj.forEach((element) => {
+                //복제본
+                const $copyOption = $originOption.clone()
+                $copyOption.empty()
+                //month객체 담아주기
+                const $monthObj = $(`<option value=${month}>${month}월</option>`).addClass('month').append(element.month);
+                $selectObj.append($monthObj)
+            })
+
+
+            //테이블에 현재 월 기준 랭킹을 조회하도록 실행
             //원본 ranklist객체 찾기
             const $originRank = $('div.memberRank>table>thead>tr')
             const $tbodyObj = $('div.memberRank>table>tbody') //tbody객체 <-여기에 복사본 넣어줄 예정
@@ -28,39 +46,34 @@ $(() => {
                 const $copyRank = $originRank.clone()
                 $copyRank.empty()
 
-                const r = element.rankmap //랭킹 정보
-                console.log(r) //rankmap 안에 key:value 세트 전체 출력
+                // const r = element.rankmap //랭킹 정보
+                // console.log(r) //rankmap 안에 key:value 세트 전체 출력
                 
-                for (var key in r) {
-                    console.log(key, r[key]) //각 key와 value 출력
-                    const rdetail = r[key]
+                for (var key in element) {
+                    console.log(key)
+                    console.log(key, element[key]) //각 key와 value 출력
+                    const rdetail = element[key]
+
+                    console.log(rdetail.id, rdetail.nickname, rdetail.totalScore, rdetail.rank)
+
+                    const $trObj = $('<tr>')
                     
-                    rdetail.forEach((item) => { //각 value의 요소들 출력
-                        console.log(item.id, item.nickname, item.totalScore, item.rank)
-                        rdetail.sort((a,b) => {
-                            return (a.rank - b.rank)
-                        })
+                    const $profileObj = $('<td>').addClass('profile').append(rdetail.profile)
+                    const $nicknameObj = $('<td>').addClass('nickname').append(rdetail.nickname)
+                    const $totalScoreObj = $('<td>').addClass('totalScore').append(rdetail.totalScore)
+                    const $rankObj = $('<td>').addClass('rank').append(rdetail.rank)
 
-                        const $trObj = $('<tr>')
-                        
-                        const $profileObj = $('<td>').addClass('profile').append(item.profile)
-                        const $nicknameObj = $('<td>').addClass('nickname').append(item.nickname)
-                        const $totalScoreObj = $('<td>').addClass('totalScore').append(item.totalScore)
-                        const $rankObj = $('<td>').addClass('rank').append(item.rank)
+                    $trObj.append($profileObj, $nicknameObj, $totalScoreObj, $rankObj)
 
-                        $trObj.append($profileObj, $nicknameObj, $totalScoreObj, $rankObj)
-
-                        $tbodyObj.append($trObj)                        
-                    })
+                    $tbodyObj.append($trObj) 
 
                 }
             })
-
         }
-
     })
+    //---- 팀 내 개인랭킹 클릭했을 때 발생할 일 END ----
 
-    //월별 랭킹보기를 위해 select박스 실행
+    //---- select박스에서 월 선택하는 change 이벤트가 발생했을 때 할 일 START ----
     selectObj.change(() => {
         const rankmonth = $('#monthSelect').val();
         console.log(rankmonth);
@@ -94,5 +107,6 @@ $(() => {
             }
         })
     })
+    //---- select박스에서 월 선택하는 change 이벤트가 발생했을 때 할 일 END ----
 
 })
